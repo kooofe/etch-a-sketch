ee8115cf18991ac8b947a73edd58e6a0e47629be
KofE <bhiushi6564@gmail.com>
2024-03-17 16:42:08 +0500

    Add style and make 16x16 pixel grid

---
+++ index.js
@@ -0,0 +1,14 @@
+// 16x16 grid using flex
+const main_container = document.querySelector('.main-container');
+
+for (let i = 0; i < 16; i++) {
+    const item_row = document.createElement("div");
+
+    for (let i = 0; i < 16; i++) {
+        const grid_item = document.createElement("div");
+        grid_item.classList.add("item");
+        item_row.appendChild(grid_item);
+    }
+    item_row.classList.add("item_row");
+    main_container.appendChild(item_row);
+}
