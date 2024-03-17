e314b0e765f4ccf29af4c0f739c7cf6f21918b7f
KofE <bhiushi6564@gmail.com>
2024-03-17 17:44:15 +0500

    Add button to color with random colors

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const main_container = document.querySelector('.main-container');
 
 size_change_btn.addEventListener('click', function () {
     main_container.innerHTML = ''
-    let grid_size = prompt("Enter grid size",16);
-    if (grid_size >= 100) grid_size = prompt("Please enter number less than 100")
+    let grid_size = prompt("Enter grid size", 16);
+    if (grid_size >= 100) grid_size = prompt("Please enter number less than 100");
     create_grid(grid_size)
 });
 
+const color_randomize_btn = document.querySelector('.color-randomize-btn');
+let randomize = false
+color_randomize_btn.addEventListener('click', function () {
+    randomize = !randomize;
+});
 
+function randomColor() {
+    return Math.floor(Math.random() * 255)
+}
 
 function create_grid(grid_length) {
     const cellSize = 800 / grid_length;
@@ -29,7 +37,11 @@ function create_grid(grid_length) {
     const grid_pixels = document.querySelectorAll(".item");
     grid_pixels.forEach((pixel) => {
         pixel.addEventListener('mouseover', () => {
-            pixel.classList.add("color");
+            if (randomize === true) {
+                pixel.style.background = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
+            } else {
+                pixel.style.background = 'blue';
+            }
         });
     });
 }
